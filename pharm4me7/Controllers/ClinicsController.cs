441f2c02bfc38547506b7e25231f764d001cd250
Sam <Sam@home>
2018-11-04 23:20:46 -0500

    added admin permissions to doctors accounts controller

---
+++ pharm4me7/Controllers/ClinicsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using pharm4me7.Models;
+
+namespace pharm4me7.Controllers
+{
+    public class ClinicsController : Controller
+    {
+        private Pharm4MeContext db = new Pharm4MeContext();
+
+        // GET: Clinics
+        public ActionResult Index()
+        {
+            return View(db.Clinics.ToList());
+        }
+
+        // GET: Clinics/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinic clinic = db.Clinics.Find(id);
+            if (clinic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinic);
+        }
+
+        // GET: Clinics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clinics/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClinicId,Name,Address,City,Phone")] Clinic clinic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clinics.Add(clinic);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clinic);
+        }
+
+        // GET: Clinics/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinic clinic = db.Clinics.Find(id);
+            if (clinic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinic);
+        }
+
+        // POST: Clinics/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClinicId,Name,Address,City,Phone")] Clinic clinic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clinic).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clinic);
+        }
+
+        // GET: Clinics/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinic clinic = db.Clinics.Find(id);
+            if (clinic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinic);
+        }
+
+        // POST: Clinics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Clinic clinic = db.Clinics.Find(id);
+            db.Clinics.Remove(clinic);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ pharm4me7/Controllers/PharmaciesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using pharm4me7.Models;
+
+namespace pharm4me7.Controllers
+{
+    public class PharmaciesController : Controller
+    {
+        private Pharm4MeContext db = new Pharm4MeContext();
+
+        // GET: Pharmacies
+        public ActionResult Index()
+        {
+            return View(db.Pharmacies.ToList());
+        }
+
+        // GET: Pharmacies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pharmacy pharmacy = db.Pharmacies.Find(id);
+            if (pharmacy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pharmacy);
+        }
+
+        // GET: Pharmacies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pharmacies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PharmacyId,Name,Address,City,Phone")] Pharmacy pharmacy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pharmacies.Add(pharmacy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pharmacy);
+        }
+
+        // GET: Pharmacies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pharmacy pharmacy = db.Pharmacies.Find(id);
+            if (pharmacy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pharmacy);
+        }
+
+        // POST: Pharmacies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PharmacyId,Name,Address,City,Phone")] Pharmacy pharmacy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pharmacy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pharmacy);
+        }
+
+        // GET: Pharmacies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pharmacy pharmacy = db.Pharmacies.Find(id);
+            if (pharmacy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pharmacy);
+        }
+
+        // POST: Pharmacies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pharmacy pharmacy = db.Pharmacies.Find(id);
+            db.Pharmacies.Remove(pharmacy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
